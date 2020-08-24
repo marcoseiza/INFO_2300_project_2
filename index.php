8a0ffdb2c4b1b2579bf14c47430e198d413586f3
Marcos Eizayaga <marcoseizayaga@gmail.com>
2020-08-24 12:41:33 -0400

    Change website title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>TODO</title>
+  <title>DATABASE PROJECT</title>
   <link rel="stylesheet" href="style/header.css">
   <link rel="stylesheet" href="style/main.css">
   <link rel="stylesheet" href="style/eatery-add.css">
